52b28516b292d3a1b43330f1cf87649d39e40057
Foster Curtis <foster.curtis@plaidpony.com>
2024-09-19 11:33:58 -0600

    Passed ChessBoard resetBoard test

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,4 +1,8 @@
 package chess;
+import java.util.*;
+
+import static chess.ChessGame.TeamColor.WHITE;
+import static chess.ChessPiece.PieceType.*;
 
 /**
  * A chessboard that can hold and rearrange chess pieces.
@@ -33,6 +37,42 @@ public class ChessBoard {
         return this.board[position.getColumn()-1][position.getRow()-1];
     }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int row = 0; row < 8; row++) {
+            sb.append("|");
+            for (int col = 0; col < 8; col++) {
+                String symbol = String.valueOf(board[row][col].getPieceType());
+                if (Objects.equals(symbol, "KNIGHT")) {
+                    symbol = "n";
+                } else {
+                    symbol = String.valueOf(symbol.charAt(0));
+                }
+                if (board[row][col].getTeamColor() == WHITE) {
+                    sb.append(symbol.toUpperCase());
+                } else {
+                    sb.append(symbol.toLowerCase());
+                }
+                sb.append('|');
+            }
+            sb.append('\n');
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ChessBoard that)) return false;
+        return Objects.deepEquals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
+    }
+
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
@@ -40,9 +80,16 @@ public class ChessBoard {
     public void resetBoard() {
         //Generate and add pawns
         for (int i = 0; i < 8; i++) {
-            this.addPiece(new ChessPosition(2, i+1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+            this.addPiece(new ChessPosition(2, i+1), new ChessPiece(WHITE, ChessPiece.PieceType.PAWN));
             this.addPiece(new ChessPosition(7, i+1), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
         }
-
+        //Generate back row pieces
+        ChessPiece.PieceType[] typeList = {ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK};
+        for (int i = 0; i < 8; i++) {
+            this.addPiece(new ChessPosition(1, i+1), new ChessPiece(WHITE, typeList[i]));
+        }
+        for (int i = 7; i >= 0; i--) {
+            this.addPiece(new ChessPosition(8, i+1), new ChessPiece(ChessGame.TeamColor.BLACK, typeList[i]));
+        }
     }
 }
