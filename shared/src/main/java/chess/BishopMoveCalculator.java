b42d28c9a7c78eae68cb2dc8a81d5398e223a188
Foster Curtis <foster.curtis@plaidpony.com>
2024-10-01 12:40:30 -0600

    Added Phase 1 test files and cleaned up some phase 0 code

--- shared/src/main/java/chess/BishopMoveCalculator.java
+++ shared/src/main/java/chess/BishopMoveCalculator.java
@@ -12,18 +12,13 @@ public class BishopMoveCalculator extends MoveCalculator {
     public Collection<ChessMove> calculateMoves() {
         Collection<ChessMove> moves = new ArrayList<>();
         // Direction = up and right
-        Collection<ChessMove> L1 = this.checkOneDirection(1, 1, 10);
+        moves.addAll(this.checkOneDirection(1, 1, 10));
         // Direction = up and left
-        Collection<ChessMove> L2 = this.checkOneDirection(1, -1, 10);
+        moves.addAll(this.checkOneDirection(1, -1, 10));
         // Direction = down and right
-        Collection<ChessMove> L3 = this.checkOneDirection(-1, 1, 10);
+        moves.addAll(this.checkOneDirection(-1, 1, 10));
         // Direction = down and left
-        Collection<ChessMove> L4 = this.checkOneDirection(-1, -1, 10);
-
-        moves.addAll(L1);
-        moves.addAll(L2);
-        moves.addAll(L3);
-        moves.addAll(L4);
+        moves.addAll(this.checkOneDirection(-1, -1, 10));
 
         return moves;
     }
--- shared/src/main/java/chess/KingMoveCalculator.java
+++ shared/src/main/java/chess/KingMoveCalculator.java
@@ -12,30 +12,21 @@ public class KingMoveCalculator extends MoveCalculator {
     public Collection<ChessMove> calculateMoves() {
         Collection<ChessMove> moves = new ArrayList<>();
 
-        Collection<ChessMove> L1 = this.checkOneDirection(1, 1, 1);
+        moves.addAll(this.checkOneDirection(1, 1, 1));
         // Direction = up and left
-        Collection<ChessMove> L2 = this.checkOneDirection(1, -1, 1);
+        moves.addAll(this.checkOneDirection(1, -1, 1));
         // Direction = down and right
-        Collection<ChessMove> L3 = this.checkOneDirection(-1, 1, 1);
+        moves.addAll(this.checkOneDirection(-1, 1, 1));
         // Direction = down and left
-        Collection<ChessMove> L4 = this.checkOneDirection(-1, -1, 1);
+        moves.addAll(this.checkOneDirection(-1, -1, 1));
         // Direction = up
-        Collection<ChessMove> L5 = this.checkOneDirection(1, 0, 1);
+        moves.addAll(this.checkOneDirection(1, 0, 1));
         // Direction = left
-        Collection<ChessMove> L6 = this.checkOneDirection(0, -1, 1);
+        moves.addAll(this.checkOneDirection(0, -1, 1));
         // Direction = right
-        Collection<ChessMove> L7 = this.checkOneDirection(0, 1, 1);
+        moves.addAll(this.checkOneDirection(0, 1, 1));
         // Direction = down
-        Collection<ChessMove> L8 = this.checkOneDirection(-1, 0, 1);
-
-        moves.addAll(L1);
-        moves.addAll(L2);
-        moves.addAll(L3);
-        moves.addAll(L4);
-        moves.addAll(L5);
-        moves.addAll(L6);
-        moves.addAll(L7);
-        moves.addAll(L8);
+        moves.addAll(this.checkOneDirection(-1, 0, 1));
 
         return moves;
     }
