d2c605fe44e420ab8d9f0a939905de97e23b9047
Foster Curtis <foster.curtis@plaidpony.com>
2024-11-23 07:45:29 -0700

    Enabled updating the ChessGame in the database

--- server/src/main/java/dataaccess/DBGameDAO.java
+++ server/src/main/java/dataaccess/DBGameDAO.java
@@ -79,8 +79,8 @@ public class DBGameDAO extends SqlConfig implements GameDAO {
 
     @Override
     public void updateGame(GameData gameData) throws DataAccessException {
-        String statement = "UPDATE games SET whiteUsername = ?, blackUsername = ? WHERE gameID = ?";
-        executeUpdate(statement, gameData.whiteUsername(), gameData.blackUsername(), gameData.gameID());
+        String statement = "UPDATE games SET whiteUsername = ?, blackUsername = ?, game = ? WHERE gameID = ?";
+        executeUpdate(statement, gameData.whiteUsername(), gameData.blackUsername(), gameData.game(), gameData.gameID());
     }
 
     public void deleteGame(int gameID) throws DataAccessException {
